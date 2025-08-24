e9cc5c287b8cf650d47ce1267750c8486b7886fc
Nathan <na639005@ucf.edu>
2025-08-23 21:24:18 -0400

    add getHumanChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -11,4 +11,33 @@ function getComputerChoice() {
         return "scissors"
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let choice = prompt("Choose rock, paper or scissors")
+
+    return choice
+}
+
+/**
+ * @param {string} humanChoice
+ * @param {string} computerChoice
+ */
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`)
+    if (humanChoice === computerChoice)
+        console.log("Tie!")
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log("You win!")
+            humanScore++
+        }
+        else {
+            console.log("You lost!")
+            computerScore++
+        }
+    }
+        
+}
+
+let humanScore = 0
+let computerScore = 0
