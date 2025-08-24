79a47e92c857fef13829bdd56c2bc683979bc07c
Nathan <na639005@ucf.edu>
2025-08-23 20:26:10 -0400

    add getComputerChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+function getComputerChoice() {
+    let choice = Math.random()
+
+    if (choice >= 0 && choice < 0.33)
+        return "rock"
+    else if (choice >= 0.33 && choice < 0.66)
+        return "paper"
+    else
+        return "scissors"
+}
+
+console.log(getComputerChoice())
