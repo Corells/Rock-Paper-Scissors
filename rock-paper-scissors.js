c1cdfdb59a73c19cfa051cc9fb05fbedb4d13ee1
Nathan <na639005@ucf.edu>
2025-08-23 21:38:09 -0400

    add playGame function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,50 +17,58 @@ function getHumanChoice() {
     return choice
 }
 
-/**
- * @param {string} humanChoice
- * @param {string} computerChoice
- */
-function playRound(humanChoice, computerChoice) {
-    console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`)
-    if (humanChoice === computerChoice)
-        console.log("Tie!")
+function playGame(params) {
+    /**
+     * @param {string} humanChoice
+     * @param {string} computerChoice
+     */
+    function playRound(humanChoice, computerChoice) {
+        console.log(`You chose ${humanChoice}, computer chose ${computerChoice}`)
+        if (humanChoice === computerChoice)
+            console.log("Tie!")
 
-    else if (humanChoice === "rock") {
-        if (computerChoice === "scissors") {
-            console.log("You win!")
-            humanScore++
+        else if (humanChoice === "rock") {
+            if (computerChoice === "scissors") {
+                console.log("You win!")
+                humanScore++
+            }
+            else {
+                console.log("You lost!")
+                computerScore++
+            }
         }
+        
+        else if (humanChoice === "scissors") {
+            if (computerChoice === "paper") {
+                console.log("You win!")
+                humanScore++
+            }
+            else {
+                console.log("You lost!")
+                computerScore++
+            }
+        }
+
         else {
-            console.log("You lost!")
-            computerScore++
-        }
-    }
-    
-    else if (humanChoice === "scissors") {
-        if (computerChoice === "paper") {
-            console.log("You win!")
-            humanScore++
-        }
-        else {
-            console.log("You lost!")
-            computerScore++
+            if (computerChoice === "rock") {
+                console.log("You win!")
+                humanScore++
+            }
+            else {
+                console.log("You lost!")
+                computerScore++
+            }
         }
     }
 
-    else {
-        if (computerChoice === "rock") {
-            console.log("You win!")
-            humanScore++
-        }
-        else {
-            console.log("You lost!")
-            computerScore++
-        }
+    let humanScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
     }
+
+    console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`)
 }
 
-let humanScore = 0
-let computerScore = 0
-
-playRound(getHumanChoice(), getComputerChoice())
+playGame()
